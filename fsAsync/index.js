7146f79744eed48ebdfe0e285e393585125c945d
Rikin <rikinbhalodia9504@gmail.com>
2022-03-13 16:17:23 +0530

    Initial commit

---
+++ fsAsync/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+// fs.writeFile("read.txt","today is great day",(err)=>{
+//     console.log('file is created');
+//     console.log(err);
+// })
+
+
+// fs.mkdir('rikin', (err)=>{
+// console.log(err);
+// })
+
+// fs.writeFile("./rikin/rikin.txt","hello welcome to the world of node.js",(err)=>{
+//     console.log("file created");
+// })
+
+// fs.appendFile("./rikin/rikin.txt","Hello my friend",(err)=>{
+//     console.log("adding the data");
+// })
+
+// const data = fs.readFile('./rikin/rikin.txt','utf-8',(err,data)=>{
+//     // console.log(err);
+//     console.log("reading this data successfully");
+//     console.log(data);
+// })
+
+// fs.rename('./rikin/rikin.txt','./rikin/rikinbhalodia.txt',(err)=>{
+//     console.log(err);
+// console.log("rename successfully");
+// })
+
+// fs.unlink("./rikin/rikinbhalodia.txt",(err)=>{
+    // console.log(err);
+    // console.log("file deleted");
+// })
+
+// fs.rmdir('./rikin',(err)=>{
+//     console.log(err);
+// console.log("folder deleted");
+// })
